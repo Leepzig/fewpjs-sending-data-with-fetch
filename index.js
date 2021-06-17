90a3600410c2ebfe0899408484cd8f2c26183501
andrew <neon.duncan@gmail.com>
2021-06-17 08:31:12 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Add your code here
+
+const form = () => document.querySelector("form")
+const usernameInput = () => document.querySelector("#username-input")
+const userEmailInput = () => document.querySelector("#email-input")
+
+document.addEventListener("DOMContentLoaded", () => {
+  addSubmitEvent()
+})
+
+const addSubmitEvent = () => {
+  form().addEventListener('submit', e => {
+    e.preventDefault()
+    submitData()
+  })
+}
+
+const submitData = () => {
+  const data = getUserAndEmail()
+  const fetchConfiguration = {
+    method: "POST",
+    headers: {
+      "Content-Type":"application/json",
+      "Accept":"application/json"
+    },
+    body:JSON.stringify(data)
+  }
+
+  fetch("http://localhost:3000/users", fetchConfiguration)
+  .then(response => response.json())
+  .then(data => console.log(data))
+  .catch(error => console.log(error.message))
+}
+
+const getUserAndEmail = () => {
+  const userInfo = {
+    name:usernameInput().value,
+    email:userEmailInput().value
+  }
+  return userInfo
+}
